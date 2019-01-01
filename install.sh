1bbcba95a9d60c650cb0fc730c0b1c1d549b6311
MoriKen <p595201m@mail.kyutech.jp>
2019-01-02 00:16:48 +0900

    Add app meta package

---
+++ install.sh
@@ -0,0 +1,18 @@
+# Clone cirkit_unit03_pkgs repository
+git clone https://github.com/MoriKen254/ntl_motoman_pkgs.git ../ntl_motoman_pkgs
+
+cd ../../
+
+wstool init src
+wstool merge -t src src/ntl_motoman_pkgs/.kinetic_depends.rosinstall
+wstool update -t src
+
+rosdep init
+rosdep update
+rosdep install -i -r -y --from-paths src --ignore-src
+
+catkin_make
+source devel/setup.bash
+
+atkin_make run_tests
+catkin_test_results
