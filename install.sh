3a4f4d9c78254307441cbfb6bb7cc3ff7cbbf35c
MoriKen <p595201m@mail.kyutech.jp>
2019-01-02 14:46:32 +0900

    Add travis and install scripts

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
-# Clone cirkit_unit03_pkgs repository
-git clone https://github.com/MoriKen254/ntl_motoman_pkgs.git ../ntl_motoman_pkgs
+#git clone https://github.com/MoriKen254/ntl_motoman_pkgs.git ../ntl_motoman_pkgs
 
 cd ../../
 
+# install dependent non-apt packages via wstool
 wstool init src
-wstool merge -t src src/ntl_motoman_pkgs/.kinetic_depends.rosinstall
+wstool merge -t src src/ntl_motoman_apps/.kinetic_depends.rosinstall
 wstool update -t src
 
+# install dependent apt packages via wstool
 rosdep init
 rosdep update
 rosdep install -i -r -y --from-paths src --ignore-src
 
+# build
 catkin_make
 source devel/setup.bash
 
+# test
 catkin_make run_tests
 catkin_test_results
